6c5f41c9d6f10edecbfa36181e9b52c44c9f197a
JoahansKepler <joahang23@gmail.com>
2025-11-19 00:18:58 -0600

    Polarizacion

--- polarizacion_circular_3D.py
+++ polarizacion_circular_3D.py
@@ -4,7 +4,7 @@ from matplotlib.animation import FuncAnimation
 from mpl_toolkits.mplot3d import Axes3D
 import math
 #### Aqui empieza lo mero bueno matematico y fisico
-# Parámetros de la onda
+# Parametros de la onda
 k = 10 # Numero de onda
 Amplitud_x = 1
 Amplitud_y = 1
@@ -12,17 +12,17 @@ omega_x= 1
 omega_y= 1
 fase_x = 1
 fase_y = 1
-desfase = 3*math.pi/4
+desfase = 0
 t = np.linspace(0, 6 * np.pi, 400)  # Tiempo
 
-# Ecuaciones paramétricas de la onda
+# Ecuaciones parametricas de la onda
 x = Amplitud_x * np.cos(omega_x * k - fase_x * t)
 y = Amplitud_y * np.cos(omega_y * k - fase_y * t - desfase)
-z = t          # propagación
+z = t          # propagacion
 
 
 ################# A partir de aqui todo esto es para la grafica###########
-# Configuración de la figura 3D
+# Configuracion de la figura 3D
 fig = plt.figure()
 ax = fig.add_subplot(111, projection="3d")
 
@@ -31,11 +31,11 @@ line, = ax.plot([], [], [], linewidth=2)
 # Límites y etiquetas
 ax.set_xlim(-1.2, 1.2)
 ax.set_ylim(-1.2, 1.2)
-ax.set_zlim(0, 30)
+ax.set_zlim(0, max(z))
 ax.set_xlabel("Campo electrico en X")
 ax.set_ylabel("Campo electrico en Y")
 ax.set_zlabel("Propagación (z)")
-ax.set_title("Polarización con angulo de desfase = 3pi/4")
+ax.set_title("Polarización con angulo de desfase = 0")
 
 
 def update(frame):
